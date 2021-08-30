fd2ac1945cc12ae194c08cb5045fc98d3cc67219
Don Kackman <dkackman@gmail.com>
2021-08-30 17:32:11 -0500

    moving

---
+++ src/rchia/Start/StartTasks.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using chia.dotnet;
+using chia.dotnet.console;
+
+namespace rchia.Start
+{
+    internal sealed class StartTasks : ConsoleTask
+    {
+        public StartTasks(FullNodeProxy fullNode, IConsoleMessage consoleMessage)
+            : base(consoleMessage)
+        {
+
+        }
+    }
+}
---
+++ src/rchia/Start/StartVerb.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using chia.dotnet;
+using chia.dotnet.console;
+
+using CommandLine;
+
+namespace rchia.Start
+{ 
+    [Verb("start", isDefault: true, HelpText = "Start services or service groups.")]
+    internal sealed class StartVerb
+    {
+        [Option('r', "restart", HelpText = "Restart the specified service(s)")]
+        public bool Restart { get; set; }
+    }
+}
