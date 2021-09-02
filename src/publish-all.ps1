b929e4b9c4ad937be30ea3af73913b2a40d39279
Don Kackman <dkackman@gmail.com>
2021-09-02 18:46:36 -0500

    add archive steps

--- src/publish-all.ps1
+++ src/publish-all.ps1
@@ -1,14 +1,29 @@
+$version = "1.0.0"
 Remove-Item '.\publish' -Recurse
 
+# stand alone
 dotnet publish ./rchia/rchia.csproj --configuration Release --framework net5.0 --output publish/standalone/win-x64 --self-contained True --runtime win-x64 --verbosity Normal /property:PublishTrimmed=True /property:PublishSingleFile=True /property:IncludeNativeLibrariesForSelfExtract=True /property:DebugType=None /property:DebugSymbols=False
 dotnet publish ./rchia/rchia.csproj --configuration Release --framework net5.0 --output publish/standalone/linux-x64 --self-contained True --runtime linux-x64 --verbosity Normal /property:PublishTrimmed=True /property:PublishSingleFile=True /property:IncludeNativeLibrariesForSelfExtract=True /property:DebugType=None /property:DebugSymbols=False
 dotnet publish ./rchia/rchia.csproj --configuration Release --framework net5.0 --output publish/standalone/osx.11.0-x64 --self-contained True --runtime osx.11.0-x64 --verbosity Normal /property:PublishTrimmed=True /property:PublishSingleFile=True /property:IncludeNativeLibrariesForSelfExtract=True /property:DebugType=None /property:DebugSymbols=False
 
+Compress-Archive -CompressionLevel Optimal -Path publish/standalone/win-x64/* -DestinationPath publish/rchia-$version-standalone-win-x64.zip
+Compress-Archive -CompressionLevel Optimal -Path publish/standalone/linux-x64/* -DestinationPath publish/rchia-$version-standalone-linux-x64.zip
+Compress-Archive -CompressionLevel Optimal -Path publish/standalone/osx.11.0-x64/* -DestinationPath publish/rchia-$version-standalone-osx.11.0-x64.zip
+
+# single file but requires dotnet
 dotnet publish ./rchia/rchia.csproj --configuration Release --framework net5.0 --output publish/singlefile/win-x64 --self-contained False --runtime win-x64 --verbosity Normal /property:PublishSingleFile=True /property:IncludeNativeLibrariesForSelfExtract=True /property:DebugType=None /property:DebugSymbols=False
 dotnet publish ./rchia/rchia.csproj --configuration Release --framework net5.0 --output publish/singlefile/linux-x64 --self-contained False --runtime linux-x64 --verbosity Normal /property:PublishSingleFile=True /property:IncludeNativeLibrariesForSelfExtract=True /property:DebugType=None /property:DebugSymbols=False
 dotnet publish ./rchia/rchia.csproj --configuration Release --framework net5.0 --output publish/singlefile/osx.11.0-x64 --self-contained False --runtime osx.11.0-x64 --verbosity Normal /property:PublishSingleFile=True /property:IncludeNativeLibrariesForSelfExtract=True /property:DebugType=None /property:DebugSymbols=False
 
+Compress-Archive -CompressionLevel Optimal -Path publish/singlefile/win-x64/* -DestinationPath publish/rchia-$version-singlefile-win-x64.zip
+Compress-Archive -CompressionLevel Optimal -Path publish/singlefile/linux-x64/* -DestinationPath publish/rchia-$version-singlefile-linux-x64.zip
+Compress-Archive -CompressionLevel Optimal -Path publish/singlefile/osx.11.0-x64/* -DestinationPath publish/rchia-$version-singlefile-osx.11.0-x64.zip
 
+# loose
 dotnet publish ./rchia/rchia.csproj --configuration Release --framework net5.0 --output publish/loose/win-x64 --self-contained False --runtime win-x64 --verbosity Normal /property:PublishSingleFile=False /property:DebugType=None /property:DebugSymbols=False
 dotnet publish ./rchia/rchia.csproj --configuration Release --framework net5.0 --output publish/loose/linux-x64 --self-contained False --runtime linux-x64 --verbosity Normal /property:PublishSingleFile=False /property:DebugType=None /property:DebugSymbols=False
 dotnet publish ./rchia/rchia.csproj --configuration Release --framework net5.0 --output publish/loose/osx.11.0-x64 --self-contained False --runtime osx.11.0-x64 --verbosity Normal  /property:PublishSingleFile=False /property:DebugType=None /property:DebugSymbols=False
+
+Compress-Archive -CompressionLevel Optimal -Path publish/loose/win-x64/* -DestinationPath publish/rchia-$version-win-x64.zip
+Compress-Archive -CompressionLevel Optimal -Path publish/loose/linux-x64/* -DestinationPath publish/rchia-$version-linux-x64.zip
+Compress-Archive -CompressionLevel Optimal -Path publish/loose/osx.11.0-x64/* -DestinationPath publish/rchia-$version-osx.11.0-x64.zip
