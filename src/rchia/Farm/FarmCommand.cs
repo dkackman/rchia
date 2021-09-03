1b8a24c9df31f3099af16c6fd02e2fe39d8b6877
Don Kackman <dkackman@gmail.com>
2021-09-03 10:39:55 -0500

    summary sub command

--- src/rchia/Farm/FarmCommand.cs
+++ src/rchia/Farm/FarmCommand.cs
@@ -1,48 +1,14 @@
-﻿using System;
-using System.Threading.Tasks;
-
-using chia.dotnet;
-
-using rchia.Commands;
-using rchia.Endpoints;
+﻿using rchia.Commands;
 
 namespace rchia.Farm
 {
     [Command("farm", Description = "Manage your farm.\nRequires a daemon endpoint.")]
-    internal sealed class FarmCommand : SharedOptions
+    internal sealed class FarmCommand
     {
         [Command("challenges", Description = "Show the latest challenges")]
         public ChallengesCommand Challenges { get; set; } = new();
 
-        [Option("s", "summary", Description = "Summary of farming information")]
-        public bool Summary { get; set; }
-
-        [CommandTarget]
-        public override async Task<int> Run()
-        {
-            try
-            {
-                using var rpcClient = await ClientFactory.Factory.CreateWebSocketClient(this, ServiceNames.Daemon);
-                var daemon = new DaemonProxy(rpcClient, ClientFactory.Factory.OriginService);
-                var tasks = new FarmTasks(daemon, this);
-
-                if (Summary)
-                {
-                    await tasks.Summary();
-                }
-                else
-                {
-                    throw new InvalidOperationException("Unrecognized command");
-                }
-
-                return 0;
-            }
-            catch (Exception e)
-            {
-                Message(e);
-
-                return -1;
-            }
-        }
+        [Command("summary", Description = "Summary of farming information")]
+        public SummaryCommand Summary { get; set; } = new();
     }
 }
---
+++ src/rchia/Farm/SummaryCommand.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using chia.dotnet;
+
+using rchia.Commands;
+using rchia.Endpoints;
+
+namespace rchia.Farm
+{
+    internal sealed class SummaryCommand : SharedOptions
+    {
+        [CommandTarget]
+        public override async Task<int> Run()
+        {
+            try
+            {
+                using var rpcClient = await ClientFactory.Factory.CreateWebSocketClient(this, ServiceNames.Daemon);
+                var daemon = new DaemonProxy(rpcClient, ClientFactory.Factory.OriginService);
+                var tasks = new FarmTasks(daemon, this);
+
+                await tasks.Summary();
+
+                return 0;
+            }
+            catch (Exception e)
+            {
+                Message(e);
+
+                return -1;
+            }
+        }
+    }
+}
