48b6748d43996d82b0155f64e4d3a37800bd24b1
Don Kackman <dkackman@gmail.com>
2021-09-06 21:41:02 -0500

    editorconfig

---
+++ src/rchia/Farm/Extensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+
+namespace rchia.Farm
+{
+
+    internal static class Extensions
+    {
+        public static string FormatTimeSpan(this TimeSpan t)
+        {
+            var builder = new StringBuilder();
+            if (t.Days > 0)
+            {
+                _ = builder.Append($"{t.Days} day{(t.Days > 1 ? "s" : "")} ");
+            }
+
+            if (t.Hours > 0)
+            {
+                _ = builder.Append($"{t.Hours} hour{(t.Hours > 1 ? "s" : "")} ");
+            }
+
+            if (t.Minutes > 0)
+            {
+                _ = builder.Append($"{t.Minutes} minute{(t.Minutes > 1 ? "s" : "")} ");
+            }
+
+            return builder.ToString();
+        }
+    }
+}
--- src/rchia/Farm/FarmTasks.cs
+++ src/rchia/Farm/FarmTasks.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Linq;
 using System.Numerics;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using chia.dotnet;
@@ -152,30 +151,6 @@ namespace rchia.Farm
             {
                 Console.WriteLine("Note: log into your key using 'chia wallet show' to see rewards for each key");
             }
-        }
-    }
-
-    public static class Extensions
-    {
-        public static string FormatTimeSpan(this TimeSpan t)
-        {
-            var builder = new StringBuilder();
-            if (t.Days > 0)
-            {
-                _ = builder.Append($"{t.Days} day{(t.Days > 1 ? "s" : "")} ");
-            }
-
-            if (t.Hours > 0)
-            {
-                _ = builder.Append($"{t.Hours} hour{(t.Hours > 1 ? "s" : "")} ");
-            }
-
-            if (t.Minutes > 0)
-            {
-                _ = builder.Append($"{t.Minutes} minute{(t.Minutes > 1 ? "s" : "")} ");
-            }
-
-            return builder.ToString();
         }
     }
 }
