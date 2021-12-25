6b5e0fd6b4537501908ffdfc4698e50ff6ed066b
Don Kackman <dkackman@gmail.com>
2021-12-25 10:36:52 -0600

    lint

--- src/rchia/Extensions.cs
+++ src/rchia/Extensions.cs
@@ -41,17 +41,17 @@ internal static class Extensions
         var builder = new StringBuilder();
         if (t.Days > 0)
         {
-            _ = builder.Append($"{t.Days} day{(t.Days > 1 ? "s" : "")} ");
+            _ = builder.Append($"{t.Days} day{(t.Days != 1 ? "s" : "")} ");
         }
 
         if (t.Hours > 0)
         {
-            _ = builder.Append($"{t.Hours} hour{(t.Hours > 1 ? "s" : "")} ");
+            _ = builder.Append($"{t.Hours} hour{(t.Hours != 1 ? "s" : "")} ");
         }
 
         if (t.Minutes > 0)
         {
-            _ = builder.Append($"{t.Minutes} minute{(t.Minutes > 1 ? "s" : "")} ");
+            _ = builder.Append($"{t.Minutes} minute{(t.Minutes != 1 ? "s" : "")} ");
         }
 
         return builder.ToString();
