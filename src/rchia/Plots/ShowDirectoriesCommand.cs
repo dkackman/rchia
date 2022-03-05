d1b32a9f9df3622e97ed7438ec1a2411bb3cd46a
Don Kackman <dkackman@gmail.com>
2022-03-05 09:45:52 -0600

    spelling

--- src/rchia/Plots/ShowDirectoriesCommand.cs
+++ src/rchia/Plots/ShowDirectoriesCommand.cs
@@ -11,14 +11,14 @@ internal sealed class ShowDirectoriesCommand : EndpointOptions
     [CommandTarget]
     public async Task<int> Run()
     {
-        return await DoWorkAsync("Retrieving plot info...", async output =>
+        return await DoWorkAsync("Retrieving directory info...", async output =>
         {
             using var rpcClient = await ClientFactory.Factory.CreateRpcClient(output, this, ServiceNames.Harvester);
             var proxy = new HarvesterProxy(rpcClient, ClientFactory.Factory.OriginService);
 
             output.WriteLine("Directories where plots are being searched for:");
             output.WriteMessage("Note that subdirectories must be added manually", false);
-            output.WriteMarkupLine("Add with '[grey]chia plots add <dir>[/]' and remove with '[grey]chia plots remove <dir>[/]'");
+            output.WriteMarkupLine("Add with '[grey]rchia plots add <dir>[/]' and remove with '[grey]rchia plots remove <dir>[/]'");
 
             using var cts = new CancellationTokenSource(TimeoutMilliseconds);
             var dirs = from path in await proxy.GetPlotDirectories(cts.Token)
